48e78ac7549ccca2f33a70892412e1a11efc65b4
Ghulam muhiuddin <muhiuddinghulam825@gmail.com>
2024-02-18 18:08:37 +0500

    add some change

--- Meme-genrator/src/app/detail/[id]/page.js
+++ Meme-genrator/src/app/detail/[id]/page.js
@@ -1,12 +1,13 @@
 'use client';
 
 import React, { useEffect, useState } from 'react';
-import { getMemes } from '@/config/api';
+import { getMemes, getOutputImageData } from '@/config/api';
 import Image from 'next/image';
 import './style.css';
 import Navbar from '@/app/Navbar';
+import Loading from '@/app/loading';
 
-async function detail({ params: { id } }) {
+function Detail({ params: { id } }) {
 
   const [outputImage, setOutputImage] = useState();
   const [result, setResult] = useState([]);
@@ -27,31 +28,57 @@ async function detail({ params: { id } }) {
 
     const response = res.data.memes.filter(element => element.id == id);
 
+    const memeImage = localStorage.getItem('user-meme');
+    const imageRes = JSON.parse(memeImage) ? JSON.parse(memeImage) : [];
+
+    const image = imageRes.filter(element => element.id == id);
+
+    image[0] && setOutputImage(image[0].url);
+
     setResult(response);
   };
 
   const changeImage = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`https://api.imgflip.com/caption_image?template_id=${id}&username=GhulamMuhiuddin&password=786muhiuddin786&text0=${e.target[0].value}&text1=${e.target[1].value}`);
+    const response = await getOutputImageData(id, e.target[0].value, e.target[1].value);
 
-    const response = await res.json();
-    
+    const memeImage = localStorage.getItem('user-meme');
+    const imageRes = JSON.parse(memeImage) ? JSON.parse(memeImage) : [];
+
+    const image = imageRes.filter(element => element.id == id);
+
+    const indexOfImage = imageRes.indexOf(image[0]);
+
+    indexOfImage == -1 ? imageRes.push({
+      url: response.data.url,
+      id: id
+    }) :
+    imageRes.splice(indexOfImage, 1, {
+        url: response.data.url,
+        id: id
+    });
+
+    localStorage.setItem('user-meme', JSON.stringify(imageRes));
+
     setOutputImage(response.data.url);
   };
-  
+
+  if (!result[0]?.url) {
+    return <Loading />
+  };
+
   return (
     <>
       <Navbar />
       <div className='edit-meme-container'>
-
-        <Image style={imageStyle} width={500}
-          height={100} src={result[0]?.url} alt='meme image' />
+        {result[0]?.url && <Image style={imageStyle} width={500}
+          height={100} src={result[0]?.url} alt='meme image' />}
 
         <form onSubmit={changeImage}>
-          <input required placeholder='Enter first text'/>
+          <input required placeholder='Enter first text' />
           <br />
-          <input required placeholder='Enter second text'/>
+          <input required placeholder='Enter second text' />
           <br />
           <button type='submit'>Generate Meme</button>
         </form>
@@ -61,7 +88,7 @@ async function detail({ params: { id } }) {
 
       </div>
     </>
-  )
-}
+  );
+};
 
-export default detail;
+export default Detail;
